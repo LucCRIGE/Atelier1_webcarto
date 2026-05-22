2a4dccc6c0d532d2f670c3020d94c55264ced13c
LucCRIGE <luc.mabire@crige-paca.org>
2026-05-22 16:50:53 +0200

    Add files via upload
    Ajout de la carte Qgis2web Leaflet

---
+++ bateau_bus_etangeberre_leaflet/data/Rseaudebateaubus_3.js
@@ -0,0 +1,29 @@
+var json_Rseaudebateaubus_3 = {
+"type": "FeatureCollection",
+"name": "Rseaudebateaubus_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"xy_coordinate_resolution": 0.0001,
+"features": [
+{ "type": "Feature", "properties": { "nb_passager_j": "3927" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0049, 43.5159 ], [ 5.0429, 43.402 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1924" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0429, 43.402 ], [ 5.118, 43.5134 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1100" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.118, 43.5134 ], [ 5.058, 43.4101 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1821" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.058, 43.4101 ], [ 5.1035, 43.4018 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "2664" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1035, 43.4018 ], [ 5.1371, 43.3998 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1284" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1371, 43.3998 ], [ 5.1861, 43.483 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "2651" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1861, 43.483 ], [ 5.2235, 43.4792 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1259" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.2235, 43.4792 ], [ 4.9995, 43.5054 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "4103" }, "geometry": { "type": "LineString", "coordinates": [ [ 4.9995, 43.5054 ], [ 5.1976, 43.4382 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "3179" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1976, 43.4382 ], [ 5.2235, 43.4508 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "4883" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.2235, 43.4508 ], [ 5.1362, 43.3994 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "4457" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1362, 43.3994 ], [ 5.1366, 43.4 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "4060" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1366, 43.4 ], [ 5.1364, 43.3999 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "2240" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1364, 43.3999 ], [ 5.0327, 43.5449 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1308" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0327, 43.5449 ], [ 4.9983, 43.4983 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "3218" }, "geometry": { "type": "LineString", "coordinates": [ [ 4.9983, 43.4983 ], [ 5.0617, 43.4011 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "4458" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0617, 43.4011 ], [ 5.1685, 43.4716 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "1600" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.1685, 43.4716 ], [ 5.0788, 43.5269 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "471" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0788, 43.5269 ], [ 5.0526, 43.4062 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "2349" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0526, 43.4062 ], [ 5.0313, 43.5482 ] ] } },
+{ "type": "Feature", "properties": { "nb_passager_j": "4287" }, "geometry": { "type": "LineString", "coordinates": [ [ 5.0313, 43.5482 ], [ 5.1184, 43.4034 ] ] } }
+]
+}
